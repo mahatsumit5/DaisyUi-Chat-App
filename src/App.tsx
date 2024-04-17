f348dfa85611669c5804ed4f8ab89bf28424644a
Sumit Mahat <mahatsumit5@gmail.com>
2024-04-17 22:27:25 +1000

    project initialized

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="h-screen bg-slate-600">
+      <button className="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
+        Button
+      </button>
+    </div>
+  );
+}
