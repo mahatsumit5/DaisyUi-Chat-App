ce886c5bc6033efd9ed2fa70efe8f746b6c7817c
Sumit Mahat <mahatsumit5@gmail.com>
2024-05-25 00:10:58 +1000

    framer motion added

--- src/components/modal/Dialog.tsx
+++ src/components/modal/Dialog.tsx
@@ -2,29 +2,45 @@ import { RxCross1 } from "react-icons/rx";
 import { useAppDispatch, useAppSelector } from "../../hook";
 import Profile from "../messages/Profile";
 import { toggleDialog } from "../../redux-slice/dialog.slice";
-
+import { motion, AnimatePresence } from "framer-motion";
+const variants = {
+  open: { opacity: 1, scale: 1 },
+  closed: { opacity: 0, scale: 0 },
+};
 const Dialog = () => {
   const dispatch = useAppDispatch();
   const { open } = useAppSelector((store) => store.dialog);
-  return open ? (
-    <div className="h-screen absolute w-screen bg-black/35 flex justify-center items-center transition-all">
-      <div className="w-[350px] bg-white rounded p-2 flex flex-col sm:min-w-[500px]">
-        <div className="flex justify-end">
-          <button
-            className="btn btn-circle btn-ghost"
-            onClick={() => {
-              dispatch(toggleDialog());
-            }}
-          >
-            <RxCross1 />
-          </button>
-        </div>
-        <div className="min-h-52">
-          <Profile type="allUsers" />
-        </div>
-      </div>
-    </div>
-  ) : null;
+  return (
+    <AnimatePresence>
+      <motion.div
+        className="h-screen absolute w-screen bg-black/55 flex justify-center items-center transition-all"
+        animate={open ? "open" : "closed"}
+        variants={variants}
+      >
+        <motion.div
+          className="w-[350px] bg-white rounded-xl p-2 flex flex-col sm:min-w-[500px] shadow-lg"
+          initial={{ opacity: 0, scale: 0 }}
+          transition={{ duration: 0.3 }}
+          animate={open ? "open" : "closed"}
+          variants={variants}
+        >
+          <div className="flex justify-end">
+            <button
+              className="btn btn-circle btn-ghost"
+              onClick={() => {
+                dispatch(toggleDialog());
+              }}
+            >
+              <RxCross1 />
+            </button>
+          </div>
+          <div className="min-h-52">
+            <Profile type="allUsers" />
+          </div>
+        </motion.div>
+      </motion.div>
+    </AnimatePresence>
+  );
 };
 
 export default Dialog;
