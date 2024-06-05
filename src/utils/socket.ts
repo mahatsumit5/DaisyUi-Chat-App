1264685d08d081d00ae7d5d85a66bf47e46d76f4
Sumit Mahat <mahatsumit5@gmail.com>
2024-06-06 02:55:20 +1000

    possible vercel fix and socket io

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -1,7 +1,9 @@
 import { io } from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = "http://localhost:8080/";
+const URL = !import.meta.env.DEV
+  ? "http://localhost:8080"
+  : import.meta.env.VITE_ROOTSERVER;
 
 export const socket = io(URL, {
   //   autoConnect: false,
