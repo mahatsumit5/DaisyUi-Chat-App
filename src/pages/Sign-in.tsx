cd288110d625d93118e12589640b872e86db87a0
Sumit Mahat <mahatsumit5@gmail.com>
2024-06-16 23:39:02 +1000

    login page design

--- src/pages/Sign-in.tsx
+++ src/pages/Sign-in.tsx
@@ -6,6 +6,8 @@ import { useAppDispatch, useAppSelector } from "../hook";
 import { useGetLoggedInUserQuery, useLoginMutation } from "../redux";
 import { setUser } from "../redux/reducer/user.slice";
 import { users } from "../dummy_data";
+import icon from "../assets/images/icon.png";
+
 const randomUserLogin = users.map((item) => {
   return { email: item.email, password: item.password };
 });
@@ -56,6 +58,9 @@ export function SignIn() {
   }, [navigate, user, location]);
   return (
     <div className=" w-full  flex  flex-col items-center justify-center p-2 h-[100dvh] gap-5">
+      <span className="text-4xl w-24 md:w-48">
+        <img src={icon} />
+      </span>
       <p className="font-bold">Login with your socials</p>
       <div className="flex gap-4">
         <FcGoogle size={45} className="bg-slate-400 p-2 rounded-xl" />
@@ -65,7 +70,7 @@ export function SignIn() {
           className="bg-slate-400 p-2 rounded-xl"
         />
       </div>
-      <div className="flex justify-center items-center gap-4 w-[400px] ">
+      <div className="flex justify-center items-center gap-4 w-screen md:w-[400px] ">
         <span className="border h-0 flex-1" />
         <p className="font-serif">or</p>
         <span className="border h-0 flex-1" />
@@ -77,7 +82,7 @@ export function SignIn() {
         <input
           type="text"
           placeholder={randomUserLogin[0].email}
-          className="p-2 px-3  bg-slate-500 rounded-md"
+          className="p-2 px-3  bg-slate-500 rounded-md text-white"
           name="email"
           onChange={onChange}
           value={form.email}
@@ -86,7 +91,7 @@ export function SignIn() {
         <input
           type="password"
           placeholder={randomUserLogin[0].password}
-          className="p-2 px-3 bg-slate-500 rounded-md"
+          className="p-2 px-3 bg-slate-500 rounded-md text-white"
           name="password"
           onChange={onChange}
           value={form.password}
@@ -108,9 +113,7 @@ export function SignIn() {
       </span>
       <span>Sign up, find friends and chat with your friends.</span>
       <Link to={"/sign-up"}>
-        <button className="rounded-full bg-white p-3 text-black px-8">
-          Join Now
-        </button>
+        <button className="btn  btn-ghost">Join Now</button>
       </Link>
     </div>
   );
