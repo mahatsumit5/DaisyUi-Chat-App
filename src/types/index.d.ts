ede80a49b5541608f6890b8c22fb092fc781ac47
Sumit Mahat <mahatsumit5@gmail.com>
2024-06-21 02:51:36 +1000

    Sucessfully chnaged types for rtkQueryErrorHAndle middleware

---
+++ src/types/index.d.ts
@@ -0,0 +1,17 @@
+// custom-typings.d.ts
+
+import { Dispatch, MiddlewareAPI } from "@reduxjs/toolkit";
+
+// Define the type for your middleware function
+type CustomMiddleware = <S, D extends Dispatch>(
+  api: MiddlewareAPI<D, S>
+) => (
+  next: (action: { payload: { data: { message: string } } }) => unknown
+) => (action: { payload: { data: { message: string } } }) => unknown;
+
+// Extend the Middleware type with your custom type
+declare module "@reduxjs/toolkit" {
+  export interface Middleware {
+    rtkQueryErrorLogger: CustomMiddleware;
+  }
+}
--- src/utils/errorHandler.tsx
+++ src/utils/errorHandler.tsx
@@ -1,24 +1,25 @@
 import { isRejectedWithValue } from "@reduxjs/toolkit";
-import type { MiddlewareAPI, Middleware } from "@reduxjs/toolkit";
+import type { Middleware, MiddlewareAPI } from "@reduxjs/toolkit";
 import { toggleToast } from "../redux/reducer/toast.slice";
 
 /**
  * Log a warning and show a toast!
  */
+
 export const rtkQueryErrorLogger: Middleware =
   (api: MiddlewareAPI) => (next) => (action) => {
     // RTK Query uses `createAsyncThunk` from redux-toolkit under the hood, so we're able to utilize these matchers!
     if (isRejectedWithValue(action)) {
-      console.warn(action);
-      if (action.payload.data.message === "jwt malformed") {
-        action.payload.data.message = "Session expired.Please log in";
-      }
+      //   if (action.payload.data.message === "jwt malformed") {
+      //     action.payload.data.message = "Session expired.Please log in";
+      //   }
+
       api.dispatch(
         toggleToast({
           isOpen: true,
           content: {
             id: Math.ceil(Math.random() * 10000000),
-            message: action.payload.data.message,
+            message: "",
 
             type: "warning",
           },
