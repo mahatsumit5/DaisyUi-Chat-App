7c7e64e76889b67b1e4f4d144f858ecd3af27d70
Sumit Mahat <mahatsumit5@gmail.com>
2024-06-29 00:30:26 +1000

    api url fixed

--- src/redux/api/serverUrl.ts
+++ src/redux/api/serverUrl.ts
@@ -1,7 +1,8 @@
 export const rootApi = !import.meta.env.PROD
-  ? "http://192.168.20.8:8080"
-  : "https://messenger-j2bf.onrender.com";
-export const userApiUrl = rootApi + "/api/v1/user";
-export const friendApiUrl = rootApi + "/api/v1/friend";
-export const roomApiUrl = rootApi + "/api/v1/room";
-export const messageApiUrl = rootApi + "/api/v1/message";
+  ? "http://localhost:8080/"
+  : import.meta.env.VITE_ROOTSERVER;
+
+export const userApiUrl = rootApi + "api/v1/user";
+export const friendApiUrl = rootApi + "api/v1/friend";
+export const roomApiUrl = rootApi + "api/v1/room";
+export const messageApiUrl = rootApi + "api/v1/message";
